2cec7dcabc885b9ce8b144937a0d04d7859593c1
damikmaxxx <damik200355555@mail.ru>
2023-02-03 18:21:57 +0300

    added style for up menu

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -2,17 +2,34 @@
   <div class="headermenu">
     <div class="left">
       <span class="server-time">12:52</span>
-      Версия 1.42.0 от 24 января 2023
+      <span class="text text-version">Версия 1.42.0 от 24 января 2023 17:33</span>
       <button class="wide">112</button>
-      <button class="wide">Оповещения</button>
+      <button class="wide">Оповещения <span class="wide__num">36</span></button>
       <button class="wide">Подписать</button>
       <button class="wide">⚠</button>
       <button class="wide">⏰</button>
       <button class="wide">#</button>
-      <button>?</button>
+      <button class="wide">?</button>
     </div>
     <div class="right">
-      Тестовый сервер |  Чувашия системня | Полный доступ | Режим ЧС | adminSMP
+      <div class="right__info">
+        <span>⚠</span>
+        <span class="text text--danger"> Тестовый сервер</span>
+        <span class="text">
+          Чувашия системня
+        </span>
+        <span class="text">
+          Полный доступ
+        </span>
+        <span class="text text--danger">
+          Режим ЧС
+        </span>
+        <span class="text">
+          adminSMP
+        </span>
+      </div>
+
+
       <el-button @click="exit()">Выход</el-button>
     </div>
   </div>
@@ -24,8 +41,8 @@ export default {
   methods: {
     exit() {
       console.log('выход',);
-      axios.post('/ambulance/logout') .then( resp => {
-        this.$router.push({path: '/login'});
+      axios.post('/ambulance/logout').then(resp => {
+        this.$router.push({ path: '/login' });
       })
     },
   },
@@ -34,38 +51,84 @@ export default {
 <style scoped>
 .headermenu {
   display: flex;
-  background: #e9e6f3;
+  background: #EFEFEF;
   height: 33px;
-  box-shadow: 0 0 3px #999;
 }
-.headermenu .left{
+
+.headermenu .left {
   width: 50%;
   display: flex;
   align-items: center;
+  padding-left: 15px;
 }
-.headermenu .right{
+
+.headermenu .right {
   display: flex;
   width: 50%;
   justify-content: right;
   align-items: center;
 }
-button{
+
+.text {
+  font-size: 18px;
+}
+.text--danger{
+  color: #D8332E;
+  font-weight: bold;
+}
+.text-version {
+  margin: 0px 5px 0px 15px;
+}
+
+button {
   height: 23px
 }
+
+.wide {
+  background-color: #D5E0EB;
+  border: none;
+  border-radius: 5px;
+  color: #646262;
+  margin: 0px 1px;
+  font-size: 18px;
+  min-width: 35px;
+  height: 100%;
+}
+
+.wide__num {
+  background: white;
+  color: red;
+  border-radius: 20px;
+  font-size: 15px;
+  padding: 2px 6px;
+}
+
 @media (max-device-width: 900px) {
-  .wide{
+  .wide {
     display: none;
+  }
+  .headermenu .left{
+    width: auto;
+  }
+  .headermenu .right{
+    width: auto;
   }
 }
 
 .server-time {
   display: inline-flex;
   border: 1px solid black;
-  padding: 0px 10px;
+  padding: 0px 12px;
   margin: 1px 10px;
   border-radius: 3px;
   font-weight: bold;
-  font-size: 26px;
+  font-size: 35px;
   line-height: 0.9;
 }
+.right__info{
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+}
+
 </style>
--- src/components/LeftMenu.vue
+++ src/components/LeftMenu.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="col">
-    <button>
-      <router-link to="/help">Help</router-link>
+    <button class="button">
+      <img src="../assets/img/icons/ambulancemonitor.png"><br>
+      <router-link class="button__text" to="/help">Help</router-link>
     </button>
-    <button>Мониторинг</button>
-    <button>
+    <button class="button button--active">
+      <img src="../assets/img/icons/ambulancemonitor.png"><br>
+      <router-link class="button__text " to="/callcard">Мониторинг</router-link>
+    </button>
+    <button class="button">
       <img src="../assets/img/icons/newKT.png"><br>
-      <router-link to="/callcard">Новый КТ</router-link>
+      <router-link class="button__text" to="/#">Новый КТ</router-link>
     </button>
-    <button>Опер. обст</button>
-    <button>Госпитализация</button>
-    <button>Поликлиника</button>
-    <button>Журнал</button>
+    <button class="button">
+      <img src="../assets/img/icons/oo.png"><br>
+      <router-link class="button__text" to="/#">Опер. обст</router-link>
+    </button>
+    <button class="button">
+      <img src="../assets/img/icons/hospital.png"><br>
+      <router-link class="button__text" to="/#">Госпитализация</router-link>
+    </button>
+    <button class="button">
+      <img src="../assets/img/icons/naryad.png"><br>
+      <router-link class="button__text" to="/#">Поликлиника</router-link>
+    </button>
+    <button class="button">
+      <img src="../assets/img/icons/naryad.png"><br>
+      <router-link class="button__text" to="/#">Журнал</router-link>
+    </button>
   </div>
 
 </template>
@@ -24,13 +40,31 @@ export default {
 .col {
   display: flex;
   flex-direction: column;
+  padding-left:20px;
 }
 
-button {
+.button {
   border: none;
-  padding: 5px 0;
-  margin: 3px;
-  background: #e9e6f3;;
-  box-shadow: 1px 1px 1px #999;
+  padding: 10px 0;
+  margin-bottom: 3px;
+  background: #e9e6f3;
+  box-shadow: 1px 1px 2px #999;
+  background: #F5F5F5;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  transition:background .3s;
+  cursor: pointer;
 }
+.button:hover{
+  background: #fff;
+}
+.button__text {
+  color: black;
+  text-decoration: none;
+  font-size: 16px;
+}
+.button--active{
+  background: #fff;
+}
+
 </style>
