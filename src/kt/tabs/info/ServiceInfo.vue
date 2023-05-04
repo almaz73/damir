6cc39c7e4cb1b2d4c94ad3af0fed416fd3bedeca
almaz <almaz_73@mail.ru>
2023-05-04 20:40:45 +0300

    datePicker position

--- src/kt/tabs/info/ServiceInfo.vue
+++ src/kt/tabs/info/ServiceInfo.vue
@@ -3,12 +3,25 @@
     <h3>Информация об обслуживании</h3>
     <el-checkbox>Показать карту</el-checkbox>
   </div>
-  <el-input/>
-  <el-date-picker/>
-  <el-date-picker/>
-  <el-date-picker/>
-
+  <div class="row">
+    <span class="label"><a>Номер вызова</a></span>
+    <el-input/>
+  </div>
+  <div class="row">
+    <span class="label"><a>Дата приема</a></span>
+    <el-date-picker/>
+  </div>
+  <div class="row">
+    <span class="label"><a>Время приема</a></span>
+    <el-date-picker/>
+  </div>
+  <div class="row">
+    <span class="label"><a>Дата смены</a></span>
+    <el-date-picker/>
+  </div>
   <el-table>
-    <td>11</td><td>22</td><td>33</td>
+    <td>11</td>
+    <td>22</td>
+    <td>33</td>
   </el-table>
 </template>
